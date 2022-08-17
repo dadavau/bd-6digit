509e44186f0b25c3da882da91a3cfd34faebfccd
Raisul Islam <110727623+dadavau@users.noreply.github.com>
2022-08-17 12:50:51 +0600

    Update bd-6digit.py

--- bd-6digit.py
+++ bd-6digit.py
@@ -96,7 +96,7 @@ def menu():
     os.system('clear')
     print ('logo')
     print ('')
-    print '\033[0;93mAUTHOR   : Raisul islam'
+    print '\033[0;93mAUTHOR   : Raisul islam
     print '\033[0;94mFACEBOOK : https://www.facebook.com/abal.not.allow.dada.is.back.R'
     print '\033[0;96mGITHUB   : dadavau'
     print ''
